b5a2056666bdfdd77f69c592a25c685683a46375
Er-VinayKumar639 <123vinaychaurasiya@gmail.com>
2024-02-29 08:16:23 +0530

    Node practice

---
+++ Backend Development/index.js
@@ -0,0 +1,11 @@
+//node REPL -> Read Evaluate Print Loop
+// node command provides environment for running js in terminal
+//we can run js files using node 
+//e.g.     node filename.js
+
+//process - This object provides information about, and control over the current node.js process.
+
+// process.argv  - returns an array containing the command-line arguements passed when the node.js process was launched
+
+
+
